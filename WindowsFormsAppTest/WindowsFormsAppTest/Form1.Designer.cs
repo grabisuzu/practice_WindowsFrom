71b496e71608af50b61bfcf82acc01785e0efb15
grabisuzu <94830417+grabisuzu@users.noreply.github.com>
2024-06-23 15:26:33 +0900

    Add files via upload

---
+++ WindowsFormsAppTest/WindowsFormsAppTest/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsAppTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.StageNumberLabel = new System.Windows.Forms.Label();
+            this.ObjectTypeBox = new System.Windows.Forms.ComboBox();
+            this.ObjectTypeLabel = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(653, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "変更";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // StageNumberLabel
+            // 
+            this.StageNumberLabel.AutoSize = true;
+            this.StageNumberLabel.Location = new System.Drawing.Point(33, 29);
+            this.StageNumberLabel.Name = "StageNumberLabel";
+            this.StageNumberLabel.Size = new System.Drawing.Size(77, 12);
+            this.StageNumberLabel.TabIndex = 4;
+            this.StageNumberLabel.Text = "Stage Number";
+            this.StageNumberLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ObjectTypeBox
+            // 
+            this.ObjectTypeBox.FormattingEnabled = true;
+            this.ObjectTypeBox.Items.AddRange(new object[] {
+            "Enemy",
+            "Ground"});
+            this.ObjectTypeBox.Location = new System.Drawing.Point(170, 46);
+            this.ObjectTypeBox.Name = "ObjectTypeBox";
+            this.ObjectTypeBox.Size = new System.Drawing.Size(121, 20);
+            this.ObjectTypeBox.TabIndex = 5;
+            this.ObjectTypeBox.Text = "ObjectType";
+            // 
+            // ObjectTypeLabel
+            // 
+            this.ObjectTypeLabel.AutoSize = true;
+            this.ObjectTypeLabel.Location = new System.Drawing.Point(168, 29);
+            this.ObjectTypeLabel.Name = "ObjectTypeLabel";
+            this.ObjectTypeLabel.Size = new System.Drawing.Size(63, 12);
+            this.ObjectTypeLabel.TabIndex = 6;
+            this.ObjectTypeLabel.Text = "ObjectType";
+            this.ObjectTypeLabel.Click += new System.EventHandler(this.ObjectTypeLabel_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(33, 151);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 21;
+            this.dataGridView.Size = new System.Drawing.Size(713, 287);
+            this.dataGridView.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(551, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "削除";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.ObjectTypeLabel);
+            this.Controls.Add(this.ObjectTypeBox);
+            this.Controls.Add(this.StageNumberLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label StageNumberLabel;
+        private System.Windows.Forms.ComboBox ObjectTypeBox;
+        private System.Windows.Forms.Label ObjectTypeLabel;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsAppTest/WindowsFormsAppTest/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+
+namespace WindowsFormsAppTest
+{
+    public partial class Form1 : Form
+    {
+        private BindingSource _bindingSource;
+        private DataTable _table;
+
+        //必要な分パスを追加する
+        private const string CSV_PATH = "csvs\\stageData.csv";
+
+        private const int MAX_ROWS = 15;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // フォームがロードされたときにCSVファイルを読み込みます
+            // 色々選択されたときに動くよう変える
+            LoadCsv(CSV_PATH);
+        }
+
+        private void LoadCsv(string filePath)
+        {
+            DataTable tempTable = new DataTable();
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                // 最初の行（ヘッダー）を読み込みます
+                string[] headers = sr.ReadLine().Split(',');
+                foreach (string header in headers)
+                {
+                    // すべての列を最初にstring型で追加します
+                    tempTable.Columns.Add(header, typeof(string));
+                }
+
+                // 残りの行を読み込みます
+                while (!sr.EndOfStream)
+                {
+                    string[] rows = sr.ReadLine().Split(',');
+                    DataRow dr = tempTable.NewRow();
+                    for (int i = 0; i < headers.Length; i++)
+                    {
+                        dr[i] = rows[i];
+                    }
+                    tempTable.Rows.Add(dr);
+
+                    // 行数をチェックし、10行を超えた場合は読み込みを中止します
+                    if (tempTable.Rows.Count >= MAX_ROWS)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            // 新しいDataTableを作成し、適切な型の列を追加します
+            DataTable dataTable = new DataTable();
+            foreach (DataColumn column in tempTable.Columns)
+            {
+                //tempTable.AsEnumerable().All()は対象のテーブルの列全てを見る,AsEnumerable()はラムダ式用の仮の変数を定義する
+                // 列のデータ全てがが "true" または "false" の場合、bool型の列として追加する
+                // tempTableの全要素をboolか否かラムダで回す,判定の出力は破棄
+                if (tempTable.AsEnumerable().All(row => bool.TryParse(row[column].ToString(), out _) || row[column].ToString()==""))
+                {
+                    dataTable.Columns.Add(column.ColumnName, typeof(bool));
+                }
+                else
+                {
+                    dataTable.Columns.Add(column.ColumnName, typeof(string));
+                }
+            }
+
+            // 新しいDataTableにデータをコピーします
+            foreach (DataRow tempRow in tempTable.Rows)
+            {
+                DataRow newRow = dataTable.NewRow();
+                foreach (DataColumn column in tempTable.Columns)
+                {
+                    if (dataTable.Columns[column.ColumnName].DataType == typeof(bool)) 
+                    {
+                        //空白の行は無視する
+                        if(tempRow[column].ToString() != "")
+                        {
+                            newRow[column.ColumnName] = Convert.ToBoolean(tempRow[column]);
+                        }
+                        
+                    }
+                    else
+                    {
+                        newRow[column.ColumnName] = tempRow[column];
+                    }
+                }
+                dataTable.Rows.Add(newRow);
+            }
+
+            // DataGridViewにDataTableをバインドします
+            dataGridView.DataSource = dataTable;
+
+            // DataGridViewの各列をチェックし、bool型の列にはチェックボックスを表示します
+            foreach (DataGridViewColumn column in dataGridView.Columns)
+            {
+                if (column.ValueType == typeof(bool))
+                {
+                    column.CellTemplate = new DataGridViewCheckBoxCell();
+                }
+            }
+
+            // イベントハンドラに処理を追加（行追加で呼び出し）
+            dataGridView.UserAddedRow += DataGridView_UserAddedRow;
+        }
+
+        private void SaveCsv(string filePath)
+        {
+            // DataGridViewのデータをDataTableとして取得します
+            DataTable dataTable = (DataTable)dataGridView.DataSource;
+
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                // カラムヘッダーを書き込みます
+                string[] columnNames = dataTable.Columns.Cast<DataColumn>()
+                                        .Select(column => column.ColumnName)
+                                        .ToArray();
+                sw.WriteLine(string.Join(",", columnNames));
+
+                // 各行のデータを書き込みます
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    // bool型の値を文字列("true"/"false")に変換して書き込みます
+                    string[] fields = row.ItemArray.Select(field =>
+                        field is bool ? (bool)field ? "true" : "false" : field.ToString())
+                                       .ToArray();
+                    sw.WriteLine(string.Join(",", fields));
+                }
+            }
+        }
+        private void DataGridView_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            // 新しい行が追加された時に行数をチェック
+            if (dataGridView.Rows.Count > MAX_ROWS + 1) // +1は新しい行のため
+            {
+                MessageBox.Show("オブジェクトの上限数に達しました。これ以上追加できません。");
+                dataGridView.Rows.RemoveAt(dataGridView.Rows.Count - 2); // 最後から2番目の行を削除（新しい行の前の行）
+            }
+        }
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            // 編集後の内容をCSVファイルに保存します
+            SaveCsv(CSV_PATH);
+        }
+
+
+        private void checkedListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ObjectTypeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 選択されている行を削除
+            foreach (DataGridViewRow row in dataGridView.SelectedRows)
+            {
+                if (row == null) return;
+                dataGridView.Rows.Remove(row);
+            }
+        }
+    }
+}
